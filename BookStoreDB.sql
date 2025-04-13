2b8c2bb803a48a6e8409dad6572e7f08bc622825
cloneyjay <jamesangatia445@gmail.com>
2025-04-13 20:34:18 +0300

    modified the columns to match the design

--- BookStoreDB.sql
+++ BookStoreDB.sql
@@ -3,23 +3,21 @@ use BookStoreDB;
 
 -- order_history table
  CREATE TABLE order_history (
-    history_id INT AUTO_INCREMENT PRIMARY KEY,
-    order_id INT NOT NULL,
-    status_id INT NOT NULL,
-    changed_at DATETIME NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES cust_order(order_id),
-    FOREIGN KEY (status_id) REFERENCES order_status(status_id)
+    order_history_id INT AUTO_INCREMENT PRIMARY KEY,
+    cust_order_id INT NOT NULL,
+    order_status_id INT NOT NULL,
+    status_changed_date DATETIME NOT NULL,
 );
 
  --  shipping_method table
  CREATE TABLE shipping_method (
-    method_id INT AUTO_INCREMENT PRIMARY KEY,
+    shipping_method_id INT AUTO_INCREMENT PRIMARY KEY,
     method_name VARCHAR(100) NOT NULL,
     cost DECIMAL(10,2) NOT NULL
 );
 
 -- order_status table
  CREATE TABLE order_status (
-    status_id INT AUTO_INCREMENT PRIMARY KEY,
+    order_status_id INT AUTO_INCREMENT PRIMARY KEY,
     status_name VARCHAR(50) NOT NULL
 );
